30b34d81e62a1d9b37a41d737a49f77f9f6e86dc
javaman1993 <64420505+javaman1993@users.noreply.github.com>
2020-04-28 17:50:52 +0300

    Add files via upload

---
+++ Primes.java
@@ -0,0 +1,20 @@
+public class Primes {
+    // проверяем является ли число простым
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // входная точка приложения
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+}
